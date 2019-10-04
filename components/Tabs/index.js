e40409580dae98ab47841e032fba28ae587995c8
JChartier001 <JChartier001@gmail.com>
2019-10-04 10:45:47 -0500

    Finished Header and working on tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        console.log(response);
+        response.data.topics.forEach(async (topics) => {
+            //setting userResponse to get usernames.  "await" sets interval.
+            const topicData = await axios.get(topics);
+            console.log(topicData)
+            topicCreator(topicData.data);
+        });
+    });
+
+        function topicCreator(array) {
+           const tab = document.createElement('div');
+
+            tab.classList.add("tab");
+
+            //tab.textContent = array.data;
+
+            return tab;
+            
+            data.topics[""0""]
+        };
+        const topicTab = document.querySelector('.topics');
+topicTab.appendChild(topicCreator());
+
+console.log(topicTab);
+
